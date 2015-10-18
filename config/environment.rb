2370ca882498ab32984418484bd99ded859740a6
hattb <hattwj@yahoo.com>
2015-10-17 20:48:44 -0700

    Fix lime/rails schema issues

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Rails.application.initialize!
+
+# Prevent legacy application tables and lime_survey tables
+# from being checked into schema.rb
+ActiveRecord::SchemaDumper.ignore_tables = [
+  /^y\d_/,
+  /p4_/,
+  /^#{LimeExt.table_prefix}/
+]
