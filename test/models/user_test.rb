24195cfea1e20f93f018a335a70693398469269b
hattb <hattwj@yahoo.com>
2015-11-23 20:45:24 -0800

    User spec tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 describe User do
+  
   it 'has a factory' do
     create :user
+    assert_equal User.count, 1
   end
+
+  it 'requires an email' do
+    user = build :user, email: nil
+    refute user.save    
+  end
+
+  it 'requires a username' do
+    user = build :user, username: nil
+    refute user.save
+  end
+
+  it 'requires a password' do
+    user = build :user, password: nil
+    refute user.save
+  end
+  
+  it 'has roles' do
+    user = build :user
+    refute user.admin?
+    user.roles = [:admin]
+    assert user.admin?
+  end
+
 end
