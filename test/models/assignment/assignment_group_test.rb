2ca0b7621d659eb37b48e4b6f05d9bafcf8ec0ad
William Eichelberger <wceichelb@gmail.com>
2016-01-27 12:18:44 -0800

    wip assignment group model tests

--- test/models/assignment/assignment_group_test.rb
+++ test/models/assignment/assignment_group_test.rb
@@ -1,13 +1,22 @@
 require "test_helper"
 
-describe Assignment::AssignmentGroup do
+describe Assignment::AssignmentGroup < ActiveSupport::TestCase do
   let(:agt) { Assignment::AssignmentGroupTemplate.find(1) }
-  let(:assignment_group) {
-    binding.pry
-    Assignment::AssignmentGroup.create(:assignment_assignment_group => agt)
-  }
+  let(:admin) { User.find(1) }
+  let(:ag) { Assignment::AssignmentGroup.new }
+  let(:agc) { agt.assignment_groups.create }
 
-  it "must be valid" do
-    value(assignment_group).must_be :valid?
+  it "must have an assignment_group_template" do
+    ag.owner = admin
+    value(ag).wont_be :valid?
+  end
+
+  it "must have an owner" do
+    ag.assignment_group_template = agt
+    value(ag).wont_be :valid?
+  end
+
+  it "should set some default values on create" do
+    agc.title.must_be :==, "Winter 2015"
   end
 end
