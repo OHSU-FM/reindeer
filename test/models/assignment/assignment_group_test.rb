20a8c6b9262720b7e92fe7093d9693597bb67dbb
William Eichelberger <wceichelb@gmail.com>
2016-02-02 16:33:09 -0800

    change assignment group model test to not be a spec

--- test/models/assignment/assignment_group_test.rb
+++ test/models/assignment/assignment_group_test.rb
@@ -1,22 +1,22 @@
 require "test_helper"
 
-describe Assignment::AssignmentGroup < ActiveSupport::TestCase do
-  let(:agt) { Assignment::AssignmentGroupTemplate.find(1) }
-  let(:admin) { User.find(1) }
+class Assignment::AssignmentGroupTest < ActiveSupport::TestCase
+  let(:agt) { assignment_assignment_group_templates(:one) }
+  let(:admin) { users(:admin) }
   let(:ag) { Assignment::AssignmentGroup.new }
   let(:agc) { agt.assignment_groups.create }
 
-  it "must have an assignment_group_template" do
+  test "must have an assignment_group_template" do
     ag.owner = admin
     value(ag).wont_be :valid?
   end
 
-  it "must have an owner" do
+  test "must have an owner" do
     ag.assignment_group_template = agt
     value(ag).wont_be :valid?
   end
 
-  it "should set some default values on create" do
-    agc.title.must_be :==, "Winter 2015"
+  test "should set some default values on create" do
+    agc.title.must_be :==, "a test assignment group template"
   end
 end
