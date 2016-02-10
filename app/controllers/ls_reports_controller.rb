4535b61292c1f6eba492319bbeda4c8bd837f21b
William Hatt <hattwj@yahoo.com>
2016-02-10 01:32:27 -0800

    Added views and controller logic

--- app/controllers/ls_reports_controller.rb
+++ app/controllers/ls_reports_controller.rb
@@ -7,8 +7,20 @@ class LsReportsController < ApplicationController
     # show all roles
     def index
         authorize! :list, LimeSurvey
-        @role_aggregates = current_user.role_aggregates
+        # List role_aggregates in order of date
+        @role_aggregates = current_user.role_aggregates.sort_by{|ra|ra.lime_survey.last_updated.to_s.to_date}
+        # Group surveys by group title
+        @survey_groups = PermissionGroups::RoleAggregateGroup.classify(@role_aggregates)
+        # Filter groups if filter present
+        if params[:filter].present?
+          @survey_groups.select!{|group|group.title == params[:filter]}
+          @role_aggregates = @survey_groups.map{|group|group.role_aggregates}.flatten
+        end
+        # Sort groups alphabetically
+        @survey_groups.sort_by{|group| group.title }
     end
+
+    protected
 
     def back_to_index
         respond_to do |format|
