3d2b68af0b3b8c10feb189fef90ce18267c2c44a
William Eichelberger <wceichelb@gmail.com>
2016-02-19 16:27:02 -0800

    ignore nil role_aggregates

--- app/models/permission_group.rb
+++ app/models/permission_group.rb
@@ -50,6 +50,9 @@ class PermissionGroup < ActiveRecord::Base
         permission_ls_groups.each do |plg|
             ra = plg.lime_survey.role_aggregate
             unless plg.ready_for_use?
+                unless ra.nil?
+                    details.push([ra.lime_survey_title, 'Permission ls group not ready for use'])
+                end
                 result.delete ra
                 next
             end
