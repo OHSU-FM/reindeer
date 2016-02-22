2cfe5564319de70123d2943cb0d41797b40764b5
William Eichelberger <wceichelb@gmail.com>
2016-02-22 10:27:31 -0800

    snag factories from feature/pinned_survey_groups

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -1,21 +1,35 @@
 FactoryGirl.define do
   factory :user do
-    username Faker::Name.first_name
-    email Faker::Internet.email
+    full_name Faker::Name.name
+    username { Faker::Internet.user_name("#{full_name}") }
+    email { Faker::Internet.email("#{username}") }
     pass = Faker::Internet.password
     password pass
-    password_confirmation pass 
-    full_name 'A test user'
-    
-    trait :default do
-      is_ldap false
-      can_dashboard true
-      can_lime true
-    end
+    password_confirmation pass
 
     trait :admin do
-      admin true
+      id 1
+      email 'test@example.com'
       superadmin true
     end
+
+    trait :coach do
+      transient do
+        coach_pg { PermissionGroup.find_by(title: 'Coach') || create(:coach_permission_group) }
+      end
+
+      can_dashboard true
+      can_lime true
+      permission_group { coach_pg }
+    end
+
+    trait :with_uex do
+      after(:build) do |usr|
+        usr.user_externals << build_list(:user_external, 1, user: usr)
+      end
+    end
+
+    factory :admin, traits: [:admin]
+    factory :coach, traits: [:coach, :with_uex]
   end
 end
