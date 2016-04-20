4a2588e847cb6a55bcac303e3ecafdbd561bc3e7
William Hatt <hattwj@yahoo.com>
2016-04-19 22:20:14 -0700

    Deleted unused helper modules and tests

--- app/helpers/assignment/user_response_helper.rb
+++
@@ -1,2 +0,0 @@
-module Assignment::UserResponseHelper
-end
--- test/helpers/assignment/survey_assignment_comments_helper_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-describe Assignment::SurveyAssignmentsHelper do
-  it "must be a real test" do
-    flunk "Need real tests"
-  end
-end
