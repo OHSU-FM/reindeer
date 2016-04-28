3b1d2a15dcbfe2ef235872a63c2572d101e19887
William Eichelberger <wceichelb@gmail.com>
2016-04-28 11:21:02 -0700

    refactor ua helper to produce buttons as well as labels, add styles for those buttons, add status indication to response_nav and show (next to ur title)

--- app/helpers/assignment/user_assignments_helper.rb
+++ app/helpers/assignment/user_assignments_helper.rb
@@ -1,21 +1,5 @@
 module Assignment::UserAssignmentsHelper
 
-  # takes list of responses, returns cumulative status
-  def category_status_label responses
-    possibilities = [
-      [ "warning", "Needs Work" ],
-      [ "info", "On Track" ],
-      [ "success", "Excelling" ]
-    ]
-    total, actual = 0, 0
-    responses.each do |ur|
-      total += ur.user_assignment.status_hash.length
-      actual += ur.user_assignment.status_hash.invert[ur.status][1].to_i - 1
-    end
-    label_kls, label_text = possibilities[(3 * (actual/total.to_f)).floor]
-    return "<span class=\"label label-#{label_kls}\">#{label_text}</span>"
-  end
-
   # returns bootstrap label type depending on resp status & possible statuses
   def status_label_color ur
     status_possibilities = ur.user_assignment.status_hash.length
@@ -33,4 +17,35 @@ module Assignment::UserAssignmentsHelper
     end
     possibilities[status_index * (status_possibilities / possibilities.length)]
   end
+
+  def category_status_label responses
+    label_kls, label_text = category_status_calculator(responses)
+    return "<span class=\"label label-#{label_kls}\">#{label_text}</span>"
+  end
+
+  def category_status_button responses, category
+    btn_kls, btn_text = category_status_calculator(responses)
+    return """
+           <button type=\"button\" class=\"btn btn-#{btn_kls} dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+             #{category.titleize}<span class=\"caret\"></span>
+           </button>
+           """
+  end
+
+  private
+
+    # takes list of resps, finds completion %, returns value on a 3-point scale
+    def category_status_calculator responses
+      possibilities = [
+        [ "warning", "Needs Work" ],
+        [ "info", "On Track" ],
+        [ "success", "Excelling" ]
+      ]
+      total, actual = 0, 0
+      responses.each do |ur|
+        total += ur.user_assignment.status_hash.length
+        actual += ur.user_assignment.status_hash.invert[ur.status][1].to_i - 1
+      end
+      kls, text = possibilities[(3 * (actual/total.to_f)).floor]
+    end
 end
