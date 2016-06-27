ab57394cb2cc22d5f30bbf80bef49939ead127f5
William Eichelberger <wceichelb@gmail.com>
2016-06-27 16:24:21 -0700

    changes to lime group and question factories, test for ua helper (need more factory changes for more tests), remove unused helper

--- spec/factories/lime_question_factory.rb
+++ spec/factories/lime_question_factory.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
   factory :lime_question do
-    qid { rand(1000..9999) }
-    question "a test question"
+    question { Faker::Lorem.sentence(3) }
     question_order 0
-    lime_group { LimeGroup.first || create(:lime_group) }
+    lime_group
   end
 end
