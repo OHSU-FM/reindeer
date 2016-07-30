b1a16b8a124c7b53435efa4013e4497c52c6127d
William Hatt <hattwj@yahoo.com>
2016-07-29 22:35:03 -0700

    - Added automatic yml configuration to docker-compose

--- build/reindeer/init.sh
+++ build/reindeer/init.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+set -e
 cd /home/app
+echo Creating config files
+/docker/create_configs.sh
+
+echo Loading rails schema
 rake db:schema:load
+
+echo Creating admin user
 rake seed:create:admin 
