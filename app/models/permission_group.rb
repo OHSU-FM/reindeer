63e34a6d2b0dc947b90666af9d238a190702b3ec
William Eichelberger <wceichelb@gmail.com>
2016-08-01 10:33:25 -0700

    change the way permission_ls_group assignment goes in rails_admin permission_group edit view

--- app/models/permission_group.rb
+++ app/models/permission_group.rb
@@ -1,123 +1,124 @@
 class PermissionGroup < ActiveRecord::Base
-    has_many :users, :inverse_of=>:permission_group
-    has_many :permission_ls_groups, :inverse_of=>:permission_group, :dependent=>:destroy
-    has_many :lime_surveys, :through=>:permission_ls_groups
-    has_many :role_aggregates, :through=>:lime_surveys
-    has_many :assignment_group_templates, ->{ active },
-      class_name: 'Assignment::AssignmentGroupTemplate'
+  has_many :users, :inverse_of=>:permission_group
+  has_many :permission_ls_groups, :inverse_of=>:permission_group, :dependent=>:destroy
+  has_many :lime_surveys, :through=>:permission_ls_groups
+  has_many :role_aggregates, :through=>:lime_surveys
+  has_many :assignment_group_templates, ->{ active },
+    class_name: 'Assignment::AssignmentGroupTemplate'
 
 
-    accepts_nested_attributes_for :permission_ls_groups, :allow_destroy=>true,
-      :reject_if=>:all_blank
-    validates_associated :permission_ls_groups
-    attr_accessible :permission_ls_groups_attributes, :allow_destroy=>true
-    attr_accessible :title, :user_ids, :pinned_survey_group_titles
-    validates :title, presence: true, uniqueness: true
+  accepts_nested_attributes_for :permission_ls_groups, :allow_destroy=>true,
+    :reject_if=>:all_blank
+  validates_associated :permission_ls_groups
+  attr_accessible :permission_ls_groups_attributes, :allow_destroy=>true
+  attr_accessible :title, :user_ids, :pinned_survey_group_titles
+  validates :title, presence: true, uniqueness: true
 
-    serialize :pinned_survey_group_titles, Array
+  serialize :pinned_survey_group_titles, Array
 
-    rails_admin do
-        navigation_label 'Permissions'
-        list do
-            field :id
-            field :title
-            field :users
+  rails_admin do
+    navigation_label 'Permissions'
+    list do
+      field :id
+      field :title
+      field :users
+    end
+    edit do
+      field :title, :string
+      field :users
+      field :permission_ls_groups do
+        label 'Surveys'
+        nested_form false
+      end
+      field :pinned_survey_group_titles do
+        label "Pinned Survey Groups"
+        help "Survey groups that are pinned to the users nav bar"
+      end
+    end
+  end
+
+  ##
+  # Names of every survey group available
+  def survey_group_titles
+    @survey_group_titles ||= survey_groups.titles
+  end
+
+  ##
+  # Enum for pinning survey groups
+  def pinned_survey_group_titles_enum
+    survey_group_titles
+  end
+
+  ##
+  # Group surveys by title
+  def survey_groups
+    @survey_groups ||= LimeExt::LimeSurveyGroup.classify(lime_surveys)
+  end
+
+  ##
+  # Only pinned surveys, grouped by title
+  def pinned_survey_groups
+    @pinned_survey_groups ||= survey_groups.filter(pinned_survey_group_titles)
+  end
+
+  ##
+  # Calculate the role_aggregate that this user can see
+  def role_aggregates_for user
+    details, result = explain_role_aggregates_for user
+    return result
+  end
+
+  ##
+  # TODO: Move to helper
+  def explain_role_aggregates_for user
+    details = []
+    result = role_aggregates.select{|ra|
+      ready = ra.ready_for_use?
+      details.push([ra, 'RA not ready for use']) unless ready
+      ready
+    }
+
+    # remove RA where user doesn't have permission (missing user externals)
+    uex = user.user_externals
+    permission_ls_groups.each do |plg|
+      ra = plg.lime_survey.role_aggregate
+      unless plg.ready_for_use?
+        unless ra.nil?
+          details.push([ra.lime_survey_title, 'Permission ls group not ready for use'])
         end
-        edit do
-            field :title, :string
-            field :users
-            field :permission_ls_groups do
-                label 'Surveys'
-            end
-            field :pinned_survey_group_titles do
-              label "Pinned Survey Groups"
-              help "Survey groups that are pinned to the users nav bar"
-            end
+        result.delete ra
+        next
+      end
+
+      plg.permission_ls_group_filters.each do |plgk|
+        next unless plgk.ident_type.present?
+        match = uex.where(:ident_type=>plgk.ident_type).first
+        if match.nil?
+          result.delete ra
+          details.push [ra,  "No User External matching: #{plgk.ident_type}"]
+          break
         end
-    end
-    
-    ##
-    # Names of every survey group available
-    def survey_group_titles
-      @survey_group_titles ||= survey_groups.titles
+      end
     end
 
-    ##
-    # Enum for pinning survey groups
-    def pinned_survey_group_titles_enum
-      survey_group_titles
-    end
-
-    ##
-    # Group surveys by title
-    def survey_groups
-      @survey_groups ||= LimeExt::LimeSurveyGroup.classify(lime_surveys)
-    end
-
-    ##
-    # Only pinned surveys, grouped by title
-    def pinned_survey_groups
-      @pinned_survey_groups ||= survey_groups.filter(pinned_survey_group_titles)
-    end
-
-    ##
-    # Calculate the role_aggregate that this user can see
-    def role_aggregates_for user
-        details, result = explain_role_aggregates_for user
-        return result
-    end
-
-    ##
-    # TODO: Move to helper
-    def explain_role_aggregates_for user
-        details = []
-        result = role_aggregates.select{|ra|
-            ready = ra.ready_for_use?
-            details.push([ra, 'RA not ready for use']) unless ready
-            ready
-        }
-
-        # remove RA where user doesn't have permission (missing user externals)
-        uex = user.user_externals
-        permission_ls_groups.each do |plg|
-            ra = plg.lime_survey.role_aggregate
-            unless plg.ready_for_use?
-                unless ra.nil?
-                    details.push([ra.lime_survey_title, 'Permission ls group not ready for use'])
-                end
-                result.delete ra
-                next
-            end
-
-            plg.permission_ls_group_filters.each do |plgk|
-                next unless plgk.ident_type.present?
-                match = uex.where(:ident_type=>plgk.ident_type).first
-                if match.nil?
-                    result.delete ra
-                    details.push [ra,  "No User External matching: #{plgk.ident_type}"]
-                    break
-                end
-            end
+    # Remove RA where user has nothing to see in the dataset ie does not have records in the dataset
+    result = result.select  do |ra|
+      begin
+        fm = LsReportsHelper::FilterManager.new user, ra.lime_survey_sid
+        lime_data = fm.lime_survey.lime_data
+        if lime_data.empty?
+          details.push [ra, 'Empty dataset after filters']
+        else
+          details.push [ra, 'Ready']
         end
-
-        # Remove RA where user has nothing to see in the dataset ie does not have records in the dataset
-        result = result.select  do |ra|
-            begin
-                fm = LsReportsHelper::FilterManager.new user, ra.lime_survey_sid
-                lime_data = fm.lime_survey.lime_data
-                if lime_data.empty?
-                    details.push [ra, 'Empty dataset after filters']
-                else
-                    details.push [ra, 'Ready']
-                end
-                !lime_data.empty?
-            rescue LsReportsHelper::AccessDenied=>e
-                details.push [ra, 'Access Denied Error']
-                false
-            end
-        end
-
-        return details, result
+        !lime_data.empty?
+      rescue LsReportsHelper::AccessDenied=>e
+        details.push [ra, 'Access Denied Error']
+        false
+      end
     end
+
+    return details, result
+  end
 
 end
--- app/models/permission_ls_group.rb
+++ app/models/permission_ls_group.rb
@@ -1,109 +1,109 @@
 class PermissionLsGroup < ActiveRecord::Base
-    belongs_to :lime_survey, :primary_key=>:sid, :foreign_key=>:lime_survey_sid, :inverse_of=>:permission_ls_groups
-    has_one :role_aggregate, :through=>:lime_survey
+  belongs_to :lime_survey, :primary_key=>:sid, :foreign_key=>:lime_survey_sid, :inverse_of=>:permission_ls_groups
+  has_one :role_aggregate, :through=>:lime_survey
 
-    belongs_to :permission_group, :inverse_of=>:permission_ls_groups
-    has_many :permission_ls_group_filters, :dependent=>:destroy, :inverse_of=>:permission_ls_group
+  belongs_to :permission_group, :inverse_of=>:permission_ls_groups
+  has_many :permission_ls_group_filters, :dependent=>:destroy, :inverse_of=>:permission_ls_group
 
-    validates_presence_of :lime_survey_sid, :permission_group_id
-    validates_presence_of :role_aggregate
-    validates_uniqueness_of :lime_survey_sid, :scope=>:permission_group_id
-    accepts_nested_attributes_for :permission_ls_group_filters, :allow_destroy=>true
-    attr_accessible :lime_survey_sid, :permission_group_id, :view_raw, :enabled, :view_all
-    attr_accessible :permission_ls_group_filters_attributes, :allow_destroy=>true
-    validate :validate_enabled_allowed
+  validates_presence_of :lime_survey_sid, :permission_group_id
+  validates_presence_of :role_aggregate
+  validates_uniqueness_of :lime_survey_sid, :scope=>:permission_group_id
+  accepts_nested_attributes_for :permission_ls_group_filters, :allow_destroy=>true
+  attr_accessible :lime_survey_sid, :permission_group_id, :view_raw, :enabled, :view_all
+  attr_accessible :permission_ls_group_filters_attributes, :allow_destroy=>true
+  validate :validate_enabled_allowed
 
-    def validate_enabled_allowed
-        if enabled && !enabled_allowed?
-            if !role_aggregate.present?
-                # Must explicitly allow view_all, or have filters
-                errors.add(:enabled, 'No role_aggregate defined')
-            elsif !role_aggregate.ready_for_use?
-                errors.add(:enabled, 'Role aggregate configuration is not complete')
+  def validate_enabled_allowed
+    if enabled && !enabled_allowed?
+      if !role_aggregate.present?
+        # Must explicitly allow view_all, or have filters
+        errors.add(:enabled, 'No role_aggregate defined')
+      elsif !role_aggregate.ready_for_use?
+        errors.add(:enabled, 'Role aggregate configuration is not complete')
+      else
+        # Must explicitly allow view_all, or have filters
+        errors.add(:enabled, 'requires "view all" or enabled filters')
+      end
+    end
+  end
+
+  def enabled_allowed?
+    return role_aggregate.present? && role_aggregate.ready_for_use? && (
+      view_all == true || permission_ls_group_filters.select{|plgf|plgf.enabled?}.count > 0
+    )
+  end
+
+  def ready_for_use?
+    enabled && enabled_allowed?
+  end
+
+  rails_admin do
+    navigation_label 'Permissions'
+    label 'Lime Survey'
+    visible false
+    list do
+      field :id do
+        pretty_value do
+          bindings[:view].link_to(bindings[:object].id, bindings[:view].edit_path(bindings[:object].class, bindings[:object].id))
+        end
+      end
+      field :enabled
+      field :ready_for_use?, :boolean do
+        read_only true
+      end
+      field :permission_group
+      field :lime_survey
+      field :view_raw
+      field :view_all
+      field :updated_at
+      field :created_at
+      sort_by "permission_group_id, lime_survey_sid"
+    end
+
+    edit do
+      field :ready_for_use?, :boolean do
+        read_only true
+      end
+
+      field :permission_group do
+        inline_add false
+        inline_edit false
+      end
+
+      field :lime_survey do
+        associated_collection_cache_all false
+        associated_collection_scope do
+          plg = bindings[:object]
+          Proc.new { |scope|
+            if plg.present?
+              scope = scope.
+                where('sid = ? OR sid not in (?)',
+                      plg.lime_survey.sid,
+                      plg.permission_group.lime_surveys.map{|ls|
+                        ls.sid})
             else
-                # Must explicitly allow view_all, or have filters
-                errors.add(:enabled, 'requires "view all" or enabled filters')
+              scope.with_role_aggregate
             end
+          }
         end
+      end
+      field :enabled
+      field :view_raw
+      field :view_all
+      field :permission_ls_group_filters do
+        label 'Survey permissions'
+      end
     end
+  end
 
-    def enabled_allowed?
-        return role_aggregate.present? && role_aggregate.ready_for_use? && (
-            view_all == true || permission_ls_group_filters.select{|plgf|plgf.enabled?}.count > 0
-        )
-    end
+  def name
+    _name = lime_survey.nil? ? 'Untitled' : lime_survey.title
+    "#{_name}#{' (disabled)' if disabled?}"
+  end
 
-    def ready_for_use?
-        enabled && enabled_allowed?
-    end
-
-    rails_admin do
-        navigation_label 'Permissions'
-        label 'Lime Survey'
-        visible false
-        list do
-            field :id do
-                pretty_value do
-                    bindings[:view].link_to(bindings[:object].id, bindings[:view].edit_path(bindings[:object].class, bindings[:object].id))
-                end
-            end
-            field :enabled
-            field :ready_for_use?, :boolean do
-                read_only true
-            end
-            field :permission_group
-            field :lime_survey
-            field :view_raw
-            field :view_all
-            field :updated_at
-            field :created_at
-            sort_by "permission_group_id, lime_survey_sid"
-        end
-
-        edit do
-            field :ready_for_use?, :boolean do
-                read_only true
-            end
-
-            field :permission_group do
-                inline_add false
-                inline_edit false
-            end
-
-            field :lime_survey do
-                associated_collection_cache_all false
-                associated_collection_scope do
-                    plg = bindings[:object]
-                    Proc.new { |scope|
-                        if plg.present?
-                            scope = scope.
-                              where('sid = ? OR sid not in (?)',
-                                    plg.lime_survey.sid,
-                                    plg.permission_group.lime_surveys.map{|ls|
-                                      ls.sid})
-                        else
-                          scope.with_role_aggregate
-                        end
-                    }
-                end
-            end
-            field :enabled
-            field :view_raw
-            field :view_all
-            field :permission_ls_group_filters do
-                label 'Survey permissions'
-            end
-        end
-    end
-
-    def name
-        _name = lime_survey.nil? ? 'Untitled' : lime_survey.title
-        "#{_name}#{' (disabled)' if disabled?}"
-    end
-
-    def disabled?
-        !ready_for_use?
-    end
+  def disabled?
+    !ready_for_use?
+  end
 
 end
 
