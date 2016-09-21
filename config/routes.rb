e8f218df5d6293823f7676ada084d8018e93616c
William Eichelberger <wceichelb@gmail.com>
2016-09-21 12:53:39 -0700

    remove some unnecessary tests and change ' to "

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,33 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+  mount RailsAdmin::Engine => "/admin", :as => "rails_admin"
 
   resources :dashboard, :controller=>:dashboard, :as=>:dashboards, :except=>[:new]
-  get 'dashboard/:id/widgets/:widget_id', :to=>'dashboard#show_widget', :constraints=>{:id=>/\d+/, :widget_id=>/\d+/}, :as=>'show_widget'
+  get "dashboard/:id/widgets/:widget_id", :to=>"dashboard#show_widget", :constraints=>{:id=>/\d+/, :widget_id=>/\d+/}, :as=>"show_widget"
 
   resources :question_widgets, :only=>[:create]
-  get 'question_widgets', :controller=>'question_widgets'
+  get "question_widgets", :controller=>"question_widgets"
 
   resources :ls_reports, :only=>[:index, :show], :param=>:sid do
     member do
-      get 'filter(/:pk(/:agg))', :action=>:show, :as=>:filter, :controller=>'ls_reports/filter',
+      get "filter(/:pk(/:agg))", :action=>:show, :as=>:filter, :controller=>"ls_reports/filter",
         :constraints=>{:pk=>/[^\/]+/, :agg=>/[^\/]+/}, :view_type=>:filter
     end
     member do
-      get 'graph(/:pk(/:agg))', :action=>:show, :as=>:graph, :controller=>'ls_reports/graph',
+      get "graph(/:pk(/:agg))", :action=>:show, :as=>:graph, :controller=>"ls_reports/graph",
         :constraints=>{:pk=>/[^\/]+/, :agg=>/[^\/]+/}, :view_type=>:graph
     end
     member do
-      get 'spreadsheet(/:pk(/:agg))', :action=>:show, :as=>:spreadsheet, :controller=>'ls_reports/spreadsheet',
+      get "spreadsheet(/:pk(/:agg))", :action=>:show, :as=>:spreadsheet, :controller=>"ls_reports/spreadsheet",
         :constraints=>{:pk=>/[^\/]+/, :agg=>/[^\/]+/}, :view_type=>:spreadsheet
     end
     member do
-      get 'instrument(/:pk(/:agg))', :action=>:show, :as=>:instrument, :controller=>'ls_reports/instrument',
+      get "instrument(/:pk(/:agg))", :action=>:show, :as=>:instrument, :controller=>"ls_reports/instrument",
         :constraints=>{:pk=>/[^\/]+/, :agg=>/[^\/]+/}
     end
     member do
-      get 'partial/:view_type/:gid(/:pk(/:agg))', :to=>'ls_reports/base#show_part', :as=>:part_of,
+      get "partial/:view_type/:gid(/:pk(/:agg))", :to=>"ls_reports/base#show_part", :as=>:part_of,
         :constraints=>{:pk=>/[^\/]+/, :agg=>/[^\/]+/, :gid=>/[^\/]+/}
     end
   end
@@ -35,8 +35,8 @@ Rails.application.routes.draw do
   resources :user, :controller=>:users, :param=>:username, :only=>[:show, :update]
 
   namespace :assignment, path: Settings.assignments_route_name  do
-    root to: 'assignment_groups#index'
-    resources :assignment_groups, param: :assignment_group_id, path: :groups, only: [:index, :show] do
+    root to: "assignment_groups#index"
+    resources :assignment_groups, param: :assignment_group_id, path: :groups, only: [:index, :show, :update] do
       resources :comments, module: :assignment_group, only: [:index, :create, :destroy]
     end
     resources :user_assignments, path: :tasks, only: [:show] do
@@ -48,21 +48,21 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'ls_files/:sid/:row_id/:qid/:name', :to=>'ls_files#show', :constraints=>{:name=>/[^\/]+/}, :as=>:lime_file
+  get "ls_files/:sid/:row_id/:qid/:name", :to=>"ls_files#show", :constraints=>{:name=>/[^\/]+/}, :as=>:lime_file
 
-  root :to=>'dashboard#index'
+  root :to=>"dashboard#index"
 
   # Error routing
-  get 'errors/file_not_found'
-  get 'errors/unprocessable'
-  get 'errors/internal_server_error'
-  get 'errors/not_authorized'
-  match '/401', to: 'errors#not_authorized', via: :all
-  match '/404', to: 'errors#file_not_found', via: :all
-  match '/422', to: 'errors#unprocessable', via: :all
-  match '/500', to: 'errors#internal_server_error', via: :all
+  get "errors/file_not_found"
+  get "errors/unprocessable"
+  get "errors/internal_server_error"
+  get "errors/not_authorized"
+  match "/401", to: "errors#not_authorized", via: :all
+  match "/404", to: "errors#file_not_found", via: :all
+  match "/422", to: "errors#unprocessable", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 
-  get 'pages/*id', to: 'high_voltage/pages#show', :as => :page, :format => false
+  get "pages/*id", to: "high_voltage/pages#show", :as => :page, :format => false
 
   match "*any", via: :all, to: "errors#file_not_found"
 
