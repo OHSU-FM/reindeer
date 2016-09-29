699556f8ac693e821d5d2a494d050d57f606f246
William Eichelberger <wceichelb@gmail.com>
2016-09-29 14:39:30 -0700

    whitespace

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,70 +1,70 @@
 class Dashboard < ActiveRecord::Base
 
-    class WidgetExpired < Exception; end
+  class WidgetExpired < Exception; end
 
-    has_paper_trail
+  has_paper_trail
 
-    belongs_to :user
-    has_many :dashboard_widgets, :inverse_of=>:dashboard, :dependent=>:destroy
-    accepts_nested_attributes_for :dashboard_widgets, :allow_destroy=>true
-    attr_accessible :theme, :dashboard_widgets_attributes
-    validates_presence_of :user
+  belongs_to :user
+  has_many :dashboard_widgets, :inverse_of=>:dashboard, :dependent=>:destroy
+  accepts_nested_attributes_for :dashboard_widgets, :allow_destroy=>true
+  attr_accessible :theme, :dashboard_widgets_attributes
+  validates_presence_of :user
 
-    rails_admin do
-        navigation_label 'User Content'
-        field :user do
-            read_only true
-        end
-        field :theme, :enum
-        field :dashboard_widgets
+  rails_admin do
+    navigation_label 'User Content'
+    field :user do
+      read_only true
     end
+    field :theme, :enum
+    field :dashboard_widgets
+  end
 
-    THEMES = [
-        'theme-waterfront-fog',
-        'theme-oregon-coast',
-        'theme-ncg-2070',
-        'theme-tillikum',
-        'theme-bokeh',
-        'theme-sunset',
-        'theme-glass',
-        'theme-fog',
-        'theme-blue-swirl',
-        'theme-grid',
-        'theme-blue-sky',
-        'theme-black']
+  THEMES = [
+    'theme-waterfront-fog',
+    'theme-oregon-coast',
+    'theme-ncg-2070',
+    'theme-tillikum',
+    'theme-bokeh',
+    'theme-sunset',
+    'theme-glass',
+    'theme-fog',
+    'theme-blue-swirl',
+    'theme-grid',
+    'theme-blue-sky',
+    'theme-black']
 
-    DEFAULT_THEME = THEMES[0]
+  DEFAULT_THEME = THEMES[0]
 
-    def title
-        "Dashboard: #{id} User:#{user.username}"
+  def title
+    "Dashboard: #{id} User:#{user.username}"
+  end
+
+  def theme_enum
+    THEMES
+  end
+
+  def theme
+    self[:theme] || DEFAULT_THEME
+  end
+
+  def validate_widget_positions
+    @widget_positions = []
+    detector = EdnaConsole::DashboardLib::CollisionDetector.new :max_cols=>5
+    dashboard_widgets.sort_by{|widget|widget.position}.each_with_index do |widget, index|
+      begin
+        indecies = detector.register_rectangle widget.position, widget.sizex, widget.sizey, index
+        new_pos = indecies.min
+        widget.position = new_pos
+        widget.col, widget.row = detector.pos_to_coord(new_pos)
+      rescue EdnaConsole::DashboardLib::Errors::ExhaustedError => e
+        widget.errors.add :position, "Unable to place widget on dashboard"
+      rescue EdnaConsole::DashboardLib::Errors::CollisionError => e
+        widget.errors.add :position, "Widget overlaps with other objects on the dashboard"
+      rescue EdnaConsole::DashboardLib::Errors::WidgetTooWideError => e
+        widget.errors.add :sizex, "Widget is too large"
+      end
     end
-
-    def theme_enum
-        THEMES
-    end
-
-    def theme
-        self[:theme] || DEFAULT_THEME
-    end
-
-    def validate_widget_positions
-        @widget_positions = []
-        detector = EdnaConsole::DashboardLib::CollisionDetector.new :max_cols=>5
-        dashboard_widgets.sort_by{|widget|widget.position}.each_with_index do |widget, index|
-            begin
-                indecies = detector.register_rectangle widget.position, widget.sizex, widget.sizey, index
-                new_pos = indecies.min
-                widget.position = new_pos
-                widget.col, widget.row = detector.pos_to_coord(new_pos)
-            rescue EdnaConsole::DashboardLib::Errors::ExhaustedError => e
-                widget.errors.add :position, "Unable to place widget on dashboard"
-            rescue EdnaConsole::DashboardLib::Errors::CollisionError => e
-                widget.errors.add :position, "Widget overlaps with other objects on the dashboard"
-            rescue EdnaConsole::DashboardLib::Errors::WidgetTooWideError => e
-                widget.errors.add :sizex, "Widget is too large"
-            end
-        end
-    end
+  end
 
 end
 
--- app/models/dashboard_widget.rb
+++ app/models/dashboard_widget.rb
@@ -1,104 +1,104 @@
 class DashboardWidget < ActiveRecord::Base
-    has_paper_trail
+  has_paper_trail
 
-    belongs_to :widget, :polymorphic=>true
-    belongs_to :dashboard, :inverse_of=>:dashboard_widgets
-    attr_accessible :position, :widget_id, :widget_type, :sizex, :sizey, :widget, :widget_title
-    attr_accessor :status
+  belongs_to :widget, :polymorphic=>true
+  belongs_to :dashboard, :inverse_of=>:dashboard_widgets
+  attr_accessible :position, :widget_id, :widget_type, :sizex, :sizey, :widget, :widget_title
+  attr_accessor :status
 
-    # Always require a dashboard
-    validates_presence_of :dashboard
-    # Only require existence of widget if one was added
-    validates_presence_of :widget, :if=> Proc.new { |o| !(o.widget_type.nil? || o.widget_id.nil?) }
-    #
-    before_save :set_status
-    after_save :set_status
-    before_destroy :optionally_delete_widget
+  # Always require a dashboard
+  validates_presence_of :dashboard
+  # Only require existence of widget if one was added
+  validates_presence_of :widget, :if=> Proc.new { |o| !(o.widget_type.nil? || o.widget_id.nil?) }
+  #
+  before_save :set_status
+  after_save :set_status
+  before_destroy :optionally_delete_widget
 
-    # Limit widget to only these types
-    WIDGET_ALLOWABLE_TYPES = ['Chart', 'QuestionWidget', 'Page']
-    DEPENDENT_DESTROY_TYPES = ['QuestionWidget']
-    EDITABLE_WIDGETS = ['Chart']
-    validates :widget_type, :inclusion=> { :in => WIDGET_ALLOWABLE_TYPES }, :allow_blank=>true
+  # Limit widget to only these types
+  WIDGET_ALLOWABLE_TYPES = ['Chart', 'QuestionWidget', 'Page']
+  DEPENDENT_DESTROY_TYPES = ['QuestionWidget']
+  EDITABLE_WIDGETS = ['Chart']
+  validates :widget_type, :inclusion=> { :in => WIDGET_ALLOWABLE_TYPES }, :allow_blank=>true
 
-    rails_admin do
-        include_all_fields
-        navigation_label 'User Content'
+  rails_admin do
+    include_all_fields
+    navigation_label 'User Content'
 
-        field :dashboard do
-            read_only true
-        end
-        field :widget_id, :enum do
-            label 'Widget Id'
-        end
-        field :widget_type, :enum
-        field :widget do
-            read_only true
-        end
-        field :sizex
-        field :sizey
-        field :position
-        field :resizeable
-        field :editable_widget? do
-            read_only true
-        end
+    field :dashboard do
+      read_only true
     end
-
-    def widget_type_enum
-        {'Chart'=>'Chart'}
+    field :widget_id, :enum do
+      label 'Widget Id'
     end
-
-    def widget_id_enum
-        return [] unless widget_type_enum.keys.include? widget_type
-        widget_type.constantize.where(:user_id=>dashboard.user_id).map do |record|
-            title = record.title.strip.empty? ? "Untitled" : record.title
-            [title, record.id]
-        end
+    field :widget_type, :enum
+    field :widget do
+      read_only true
     end
-
-    # set default position to 1
-    def position
-        self[:position] || 1
+    field :sizex
+    field :sizey
+    field :position
+    field :resizeable
+    field :editable_widget? do
+      read_only true
     end
+  end
 
-    def set_status
-        if widget_id_changed? || widget_type_changed?
-            status.push 'refresh-widget'
-        elsif status.include? 'new-record'
-            status.delete 'new-record'
-            status.push 'freshly-created'
-        end
+  def widget_type_enum
+    {'Chart'=>'Chart'}
+  end
+
+  def widget_id_enum
+    return [] unless widget_type_enum.keys.include? widget_type
+    widget_type.constantize.where(:user_id=>dashboard.user_id).map do |record|
+      title = record.title.strip.empty? ? "Untitled" : record.title
+      [title, record.id]
     end
+  end
 
-    def status
-        @status ||=[]
-        if new_record?
-            @status.push 'new-record'
-        end
-        @status.uniq!
-        @status
+  # set default position to 1
+  def position
+    self[:position] || 1
+  end
+
+  def set_status
+    if widget_id_changed? || widget_type_changed?
+      status.push 'refresh-widget'
+    elsif status.include? 'new-record'
+      status.delete 'new-record'
+      status.push 'freshly-created'
     end
+  end
 
-    def content
-
+  def status
+    @status ||=[]
+    if new_record?
+      @status.push 'new-record'
     end
+    @status.uniq!
+    @status
+  end
 
-    def editable_widget?
-        # Should we allow this widget to be edited?
-        return true if self.widget_type.to_s == ""          # Editable if nothing is set
-        return EDITABLE_WIDGETS.include?(self.widget_type)  # Editable if in editable types
-    end
+  def content
 
-    ##
-    # Dump widget to json
-    def as_json(options=nil)
-        super({:include =>{:widget=>{:methods=>:content}}}.merge(options || {}))
-    end
+  end
 
-    def optionally_delete_widget
-        return unless widget
-        widget.destroy if DEPENDENT_DESTROY_TYPES.include? widget_type
-    end
+  def editable_widget?
+    # Should we allow this widget to be edited?
+    return true if self.widget_type.to_s == ""          # Editable if nothing is set
+    return EDITABLE_WIDGETS.include?(self.widget_type)  # Editable if in editable types
+  end
 
-    deprecate :content
+  ##
+  # Dump widget to json
+  def as_json(options=nil)
+    super({:include =>{:widget=>{:methods=>:content}}}.merge(options || {}))
+  end
+
+  def optionally_delete_widget
+    return unless widget
+    widget.destroy if DEPENDENT_DESTROY_TYPES.include? widget_type
+  end
+
+  deprecate :content
 end
