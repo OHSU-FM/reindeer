d78cf8d100cf6a44d09f4f684be55562e2bef72d
Patrick V Chung <patrickvchung@gmail.com>
2020-04-28 13:57:27 +0000

    Removed create_epa_masters & create_epa_reviews & their drop migrations.

--- db/migrate/20200114161516_drop_epa_masters.rb
+++
@@ -1,5 +0,0 @@
-class DropEpaMasters < ActiveRecord::Migration[5.2]
-  def change
-	   drop_table :epa_masters
-  end
-end
--- db/migrate/20200114162853_drop_epa_reviews.rb
+++
@@ -1,5 +0,0 @@
-class DropEpaReviews < ActiveRecord::Migration[5.2]
-  def change
-    	drop_table :epa_reviews
-  end
-end
