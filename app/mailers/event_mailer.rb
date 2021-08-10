587586bc06ae5b64e4d1522d7545045cdee6b925
Patrick V Chung <patrickvchung@gmail.com>
2021-08-10 10:53:29 -0700

    OASIS - Added email feature for appointment cancellation - tested on Staging2.

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -16,13 +16,13 @@ class EventMailer < ApplicationMailer
   def notify_student_advisor_appt_cancel (meeting)
       @event_mailer = Event.find(meeting.event_id)
       @meeting_mailer = meeting
-      student_email = 'chungp@ohsu.edu'  #meeting.user.email # student email
-      #cc_email = Advisor.find_by(id: meeting.advisor_id).email  # advisor email
+      student_email = meeting.user.email # student email
+      cc_email = Advisor.find_by(id: meeting.advisor_id).email  # advisor email
       emails = []
       emails << student_email
-      #emails << cc_email
+      emails << cc_email
 
-      mail(to: emails, from: "chomina@ohsu.edu", subject: "Appointment canceled with #{@event_mailer.description} on #{@event_mailer.start_date.strftime("%m/%d/%Y %I:%M %p - %A")}")
+      mail(to: emails, from: "chomina@ohsu.edu", subject: "Your Appointment with #{@event_mailer.description} on #{@event_mailer.start_date.strftime("%m/%d/%Y %I:%M %p - %A")} has been canceled.")
 
   end
 end
